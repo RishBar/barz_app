dd2c9252f1a343fcf657084506b914c660ea2451
Rishav Baral <rishavbaral6@gmail.com>
2019-12-03 23:12:46 -0800

    add gravatar display for individual users when they sign up and go to their page, and add integrated test for valid user signup

--- test/controllers/emcees_controller_test.rb
+++ test/controllers/emcees_controller_test.rb
@@ -17,7 +17,7 @@ class EmceesControllerTest < ActionDispatch::IntegrationTest
 
   test "should create emcee" do
     assert_difference('Emcee.count') do
-      post emcees_url, params: { emcee: { email: 'example2@example.com', name: @emcee.name, password: 'imabeast1234' } }
+      post emcees_url, params: { emcee: { email: 'example2@example.com', name: @emcee.name, password: 'dude123456' } }
     end
 
     assert_redirected_to emcee_url(Emcee.last)
@@ -25,14 +25,14 @@ class EmceesControllerTest < ActionDispatch::IntegrationTest
 
   test "should not create emcee if email is invalid" do
     assert_no_difference('Emcee.count') do
-      post emcees_url, params: { emcee: { email: 'example2example.com', name: @emcee.name, password: 'imabeast1234' } }
+      post emcees_url, params: { emcee: { email: 'example2example.com', name: @emcee.name, password: 'dude123456' } }
     end
     assert_template 'new'
   end
 
   test "should not create emcee if name is invalid" do
     assert_no_difference('Emcee.count') do
-      post emcees_url, params: { emcee: { email: 'example2@example.com', name: '', password: 'imabeast1234' } }
+      post emcees_url, params: { emcee: { email: 'example2@example.com', name: '', password: 'dude123456' } }
     end
     assert_template 'new'
   end
@@ -55,7 +55,7 @@ class EmceesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update emcee" do
-    patch emcee_url(@emcee), params: { emcee: { email: @emcee.email, name: @emcee.name, password: 'imabeast1234' } }
+    patch emcee_url(@emcee), params: { emcee: { email: @emcee.email, name: @emcee.name, password: 'dude123456' } }
     assert_redirected_to emcee_url(@emcee)
   end
 
--- test/integration/emcees_signup_test.rb
+++ test/integration/emcees_signup_test.rb
@@ -4,9 +4,27 @@ class EmceesSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup" do
     get new_emcee_path
     assert_no_difference 'Emcee.count' do
-      post emcees_path, params: { emcee: {name: 'fdsa',email:'fdsainvalid.com', password:'dude', password_confirmation:'dude'}}
+      post emcees_path, params: { emcee: {name: 'fdsa',
+                                 email:'fdsainvalid.com', 
+                                 password:'dude', 
+                                 password_confirmation:'dude'}}
     end 
     assert_template 'new'
   end
+
+class EmceesSignupTest < ActionDispatch::IntegrationTest
+  test "valid signup information" do
+    get new_emcee_path
+    assert_difference 'Emcee.count', 1 do
+      post emcees_path, params: { emcee: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password123",
+                                         password_confirmation: "password123" } }
+    end
+    follow_redirect!
+    assert_template 'emcees/show'
+  end
+end
+require 'test_helper'
   
 end
